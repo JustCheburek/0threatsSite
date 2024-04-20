809348d8477b670a3a2b1c1b037b5f05e84ff937
JustCheburek <artemiznakhdki@gmail.com>
2024-04-20 14:00:21 +1000

    footer + sos

--- ui/components/footer.tsx
+++ ui/components/footer.tsx
@@ -1,15 +1,45 @@
+import Link from "next/link";
 // Стили
 import "./styles/footer.scss";
 
 export function Footer() {
-    return (
-        <footer>
-            <div className="footer">
-                {/*Копирайт*/}
-                <p className="center_text">
-                    &#169; Нулевая угроза 2024
-                </p>
-            </div>
-        </footer>
-    )
+	return (
+			<footer>
+				<div className="footer">
+					{/*Копирайт*/}
+					<p className="center_text">
+						&#169; Нулевая угроза 2024
+					</p>
+					<h4>
+						<Link href="tel:01">
+							01 или 101
+						</Link>
+					</h4>
+					<p>
+						Единый телефон пожарных и спасателей
+					</p>
+					<h4 className="all_select">
+						<Link href="tel:88127182505">
+							8 (812) 718-25-05
+						</Link>
+					</h4>
+					<p>
+						Приемная
+					</p>
+					<h4 className="all_select">
+						<Link href="tel:88122999999">
+							8 (812) 299-99-99
+						</Link>
+					</h4>
+					<p>
+						Единый «телефон доверия»
+					</p>
+					<h4 className="all_select">
+						<Link href="mailto:gu@78.mchs.gov.ru">
+							gu@78.mchs.gov.ru
+						</Link>
+					</h4>
+				</div>
+			</footer>
+	)
 }
