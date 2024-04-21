57c13f4992da23e4feaa7084757b6cff37c2f80e
JustCheburek <artemiznakhdki@gmail.com>
2024-04-21 11:01:22 +1000

    map

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -13,9 +13,9 @@ export default function Main() {
 					<h3 className="center_text">
 						Ваш голос – наша сила: вместе против терроризма
 					</h3>
-					<div className={styles.opinion}>
+					<div className={`${styles.box} ${styles.opinion}`}>
 						<ImgBox>
-							<Img src="/psychologist.png" alt="Психолог" className={styles.img} height={300}/>
+							<Img src="/psychologist.png" alt="Психолог" className={styles.img} width={300}/>
 						</ImgBox>
 						<q>
 							Когда в наш мир вторгаются такие события, как теракты, сложно остаться от этого в стороне – хотя бы в мыслях.
@@ -24,67 +24,72 @@ export default function Main() {
 					<p>
 						Обращаясь к нам вы не только можете почувствовать поддержку, но и получить информацию от профессионалов
 					</p>
-					<Form>
-						<FormLabel>
-							<FormInput
-									placeholder="Имя"
-									name="name"
-									autoComplete="name"
-									required
-							/>
-						</FormLabel>
-						<FormGroup>
+					<div className={`${styles.box} ${styles.form_box}`}>
+						<Form>
 							<FormLabel>
-								М
 								<FormInput
-										type="radio"
-										placeholder="Пол"
-										name="gender"
-										defaultChecked
-										autoComplete="gender"
+										placeholder="Имя"
+										name="name"
+										autoComplete="name"
+										required
+								/>
+							</FormLabel>
+							<FormGroup>
+								<FormLabel>
+									М
+									<FormInput
+											type="radio"
+											placeholder="Пол"
+											name="gender"
+											defaultChecked
+											autoComplete="gender"
+									/>
+								</FormLabel>
+								<FormLabel>
+									Ж
+									<FormInput
+											type="radio"
+											placeholder="Пол"
+											name="gender"
+											autoComplete="gender"
+									/>
+								</FormLabel>
+							</FormGroup>
+							<FormLabel>
+								<FormInput
+										placeholder="Возраст"
+										name="age"
+										autoComplete="age"
+										type="number"
+										required
+										min={14}
+										max={200}
 								/>
 							</FormLabel>
 							<FormLabel>
-								Ж
 								<FormInput
-										type="radio"
-										placeholder="Пол"
-										name="gender"
-										autoComplete="gender"
+										placeholder="Почта"
+										name="email"
+										autoComplete="email"
+										required
 								/>
 							</FormLabel>
-						</FormGroup>
-						<FormLabel>
-							<FormInput
-									placeholder="Возраст"
-									name="age"
-									autoComplete="age"
-									type="number"
-									required
-									min={14}
-									max={200}
-							/>
-						</FormLabel>
-						<FormLabel>
-							<FormInput
-									placeholder="Почта"
-									name="email"
-									autoComplete="email"
-									required
-							/>
-						</FormLabel>
-						<FormLabel>
-							<FormInput
-									placeholder="Адрес"
-									name="address"
-									autoComplete="address"
-									required
-							/>
-						</FormLabel>
-						<FormButton>
-							Отправить
-						</FormButton>
-					</Form>
+							<FormLabel>
+								<FormInput
+										placeholder="Адрес"
+										name="address"
+										autoComplete="address"
+										required
+								/>
+							</FormLabel>
+							<FormButton>
+								Отправить
+							</FormButton>
+						</Form>
+						<ImgBox>
+							<Img src="/map.png" alt="Карта" className={styles.img}/>
+						</ImgBox>
+					</div>
 				</MaxSize>
 			</main>
 	)
