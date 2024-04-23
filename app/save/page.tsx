77870dac6dd5a904cac0e43185bc3081bcbd2a9c
JustCheburek <artemiznakhdki@gmail.com>
2024-04-23 10:54:33 +1000

    images

--- app/save/page.tsx
+++ app/save/page.tsx
@@ -1,6 +1,7 @@
 import {MaxSize} from "@components/maxSize";
 import type {Metadata} from "next";
 import {GBox, GContainer, GText} from "@components/grid";
+import {Img, ImgBox} from "@components/img";
 
 export const metadata: Metadata = {
 	title: "Спасение | Нулевая угроза",
@@ -13,13 +14,25 @@ export default function Resources() {
 				<MaxSize>
 					<h1>Спасение</h1>
 					<GContainer border>
-						<GBox href="/save/terrorist">
+						<GBox href="/save/terrorist" imgs="one">
+							<ImgBox type="grid">
+								<Img src="/stopTerror.png" alt="Стоп террор"/>
+							</ImgBox>
+
 							<GText>При терактах</GText>
 						</GBox>
-						<GBox href="/save/rocket">
+						<GBox href="/save/rocket" imgs="one">
+							<ImgBox type="grid">
+								<Img src="/rocket.png" alt="Ракетная опасность"/>
+							</ImgBox>
+
 							<GText>При ракетной опасности</GText>
 						</GBox>
-						<GBox href="/save/nature">
+						<GBox href="/save/nature" imgs="one">
+							<ImgBox type="grid">
+								<Img src="/planet.png" alt="Природные катаклизмы"/>
+							</ImgBox>
+
 							<GText>При природных катаклизмах</GText>
 						</GBox>
 					</GContainer>
